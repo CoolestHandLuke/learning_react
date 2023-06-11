121e7a152f654b0d928827b6b6c3a11e9983e84f
Luke Shea <shealuke@yahoo.com>
2023-06-11 15:52:33 -0500

    Learned props, continued working through tutorials

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,26 +3,41 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const firstBook = {
+    title: "Spider-Man: Across the Spider-Verse: The Art of the Movie",
+    author: "Ramin Zahed",
+    img: "./images/book-1.jpg",
+};
+
+const secondBook = {
+    title: "Democracy Awakening: Notes on the State of America",
+    author: "Heather Cox Richardson",
+    img: "./images/book-2.jpg",
+};
+
 const BookList = () => {
     return (
         <section className="bookList">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book
+                author={firstBook.author}
+                title={firstBook.title}
+                img={firstBook.img}
+            />
+            <Book
+                author={secondBook.author}
+                title={secondBook.title}
+                img={secondBook.img}
+            />
         </section>
     );
 };
 
-const Book = () => {
-    const title = "Spider-Man: Across the Spider-Verse: The Art of the Movie";
-    const author = "Ramin Zahed";
+const Book = (props) => {
+    const { img, title, author } = props;
+    console.log(props);
     return (
         <article className="book">
-            <img
-                src="https://images-na.ssl-images-amazon.com/images/I/818LCPpmyWL._AC_UL900_SR900,600_.jpg"
-                alt="Spider-Man"
-            />
+            <img src={img} alt={title} />
             <h2>{title}</h2>
             <h4>{author}</h4>
         </article>
