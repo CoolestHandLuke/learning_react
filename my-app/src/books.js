bda3b9148474b06925f44a97b4fa2c5dd09b6a0b
Luke Shea <shealuke@yahoo.com>
2023-06-11 22:30:13 -0500

    Built and deployed my first React app. Feels pretty good, really

---
+++ my-app/src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
+export const books = [
+    {
+        title: "Spider-Man: Across the Spider-Verse: The Art of the Movie",
+        author: "Ramin Zahed",
+        img: img1,
+        id: 1,
+    },
+    {
+        title: "Democracy Awakening: Notes on the State of America",
+        author: "Heather Cox Richardson",
+        img: img2,
+        id: 2,
+    },
+    {
+        title: "Fourth Wing (The Empyrean, 1)",
+        author: "Rebecca Yarros",
+        img: img3,
+        id: 3,
+    },
+];
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,45 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-
-const firstBook = {
-    title: "Spider-Man: Across the Spider-Verse: The Art of the Movie",
-    author: "Ramin Zahed",
-    img: "./images/book-1.jpg",
-};
-
-const secondBook = {
-    title: "Democracy Awakening: Notes on the State of America",
-    author: "Heather Cox Richardson",
-    img: "./images/book-2.jpg",
-};
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
     return (
-        <section className="bookList">
-            <Book
-                author={firstBook.author}
-                title={firstBook.title}
-                img={firstBook.img}
-            />
-            <Book
-                author={secondBook.author}
-                title={secondBook.title}
-                img={secondBook.img}
-            />
-        </section>
-    );
-};
-
-const Book = (props) => {
-    const { img, title, author } = props;
-    console.log(props);
-    return (
-        <article className="book">
-            <img src={img} alt={title} />
-            <h2>{title}</h2>
-            <h4>{author}</h4>
-        </article>
+        <React.Fragment>
+            <h1>amazon best sellers</h1>
+            <section className="bookList">
+                {books.map((book, index) => {
+                    return <Book {...book} key={book.id} number={index} />;
+                })}
+            </section>
+        </React.Fragment>
     );
 };
 
