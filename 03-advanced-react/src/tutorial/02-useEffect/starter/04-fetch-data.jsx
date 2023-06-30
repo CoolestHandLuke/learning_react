641129a22003e23323ab527fc9469054ce950212
Luke Shea <shealuke@yahoo.com>
2023-06-30 01:32:19 -0500

    Worked through some more turtorials, getting a better understanding of useEffect and useState

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,46 @@
-const url = 'https://api.github.com/users';
+import { useState, useEffect } from "react";
+
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const resp = await fetch(url);
+                const users = await resp.json();
+                console.log(users);
+                setUsers(users);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
+    }, []);
+    return (
+        <section>
+            <h3>Github Users</h3>
+            <ul className="users">
+                {users.map((user) => {
+                    const {
+                        avatar_url: image,
+                        login: name,
+                        url: link,
+                        id,
+                    } = user;
+                    return (
+                        <li key={id}>
+                            <img src={image} alt={name} className="img" />
+
+                            <div>
+                                <h5>{name}</h5>
+                                <a href={link}>Profile</a>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        </section>
+    );
 };
 export default FetchData;
